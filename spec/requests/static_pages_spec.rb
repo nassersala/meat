a4c34a4e7d9c75676fc3586d08c1944997e79c1e
Nasser <nassersala@gmail.com>
2012-01-31 19:17:03 +1100

    Added a contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,4 +41,17 @@ describe "Static pages" do
     end
   end
   
+  describe "Contact page" do
+    
+    it "should have the h1 'Contact Us" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact Us')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', text: 'Meet There | Contact Us')
+    end
+  end
+  
 end
