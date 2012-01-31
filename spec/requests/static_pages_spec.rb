eea4d4b64c93cf1c402e8e596fe831f2332d13ea
Nasser <nassersala@gmail.com>
2012-01-31 21:25:47 +1100

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,57 +1,31 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  
+  subject { page }
 
   describe "Home page" do
-
-    it "should have the h1 'Meet There'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', text: 'Meet There')
-    end
-    
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', text: 'Meet There | Home')
-    end
+    before { visit root_path }
+    it { should have_selector('h1', text: 'Meet There')}
+    it { should have_selector('title', text: full_title('Home')) }  
   end
 
   describe "Help page" do
-
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', text: 'Help')
-    end
-    
-    it "should have the title help" do
-      visit '/static_pages/help'
-      page.should have_selector('title', text: 'Meet There | Help')
-    end
+    before { visit help_path }
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
   
   describe "About page" do
-    
-    it "should have the h1 'About Us" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', text: 'About Us')
-    end
-    
-    it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title', text: 'Meet There | About Us')
-    end
+    before { visit about_path }
+      it { should have_selector('h1', text: 'About') }
+      it { should have_selector('title', text: full_title('About Us')) }
   end
   
   describe "Contact page" do
-    
-    it "should have the h1 'Contact Us" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', text: 'Contact Us')
-    end
-    
-    it "should have the right title" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', text: 'Meet There | Contact Us')
-    end
+    before { visit contact_path }
+      it { should have_selector('h1', text: 'Contact') }
+      it { should have_selector('title', text: full_title('Contact Us')) }
   end
   
 end
