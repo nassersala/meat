ad106efe673daebed39cfd6a7a7626e3f541878c
Nasser <nassersala@gmail.com>
2012-01-31 16:33:19 +1100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails', '2.0.0'
+
+group :development, :test do
+  gem 'sqlite3', '1.3.5'
+  gem 'rspec-rails', '2.8.1'
+end
+
+group :test do 
+  gem 'capybara', '1.1.2'
+end
+
+group :production do
+  gem 'pg', '0.12.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeetThere::Application.config.secret_token = '7d28d8d39f286b61497f0d3073b73a4b97e731bdebc693440466a8ab9f4d0c5572ba9a83e160189fa49f9ec3e32084894283917b01baf42253030bae8cfa827e'
