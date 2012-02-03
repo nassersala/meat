07f5c4896316f37c2ea0fff5ad5416e0d0b87e37
Nasser <nassersala@gmail.com>
2012-02-03 23:56:37 +1100

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  before_save :create_remember_token
   
   validates :name,  presence: true, length: { maximum: 50 }
   valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,8 +9,15 @@ class User < ActiveRecord::Base
                                     uniqueness: { case_sensitive: false}
   validates :password, presence: true
   
-end
 
+private 
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
+  
+end
+  
 # == Schema Information
 #
 # Table name: users
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 MeetThere::Application.routes.draw do
   resources :users
-  match '/signup', to: 'users#new'
-
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy'
+  
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
