21df76f6fa84201cb20d7fee355e770c6806100d
Nasser <nassersala@gmail.com>
2012-02-18 11:47:25 +1100

    added admin_user in usercontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,4 +59,8 @@ class UsersController < ApplicationController
     redirect_to(root_path) unless current_user?(@user)
   end
   
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+  
 end
